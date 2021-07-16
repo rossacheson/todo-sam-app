e103a759d64696406dd2645a99b081d9f7fad01d
Ross Acheson <racheson@fivestardev.com>
2021-07-16 12:57:53 -0400

    add create, update, delete functions

---
+++ .aws-sam/build/UpdateTodoFunction/listTodos.js
@@ -0,0 +1,35 @@
+//GET /todos => in28minutes
+//Lamdba function
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    console.log(event)
+
+    const username = 'in28minutes'
+
+    //AVOID HARDCODING
+    //HTTP API => const username = event.requestContext.authorizer.jwt.claims['cognito:username']
+    //REST API => const username = event.requestContext.authorizer.claims['cognito:username']
+
+    var params = {
+        TableName: process.env.TODO_TABLE,
+        FilterExpression: 'username = :username',
+        ExpressionAttributeValues: { ':username': username }
+    };
+
+
+    const results = await dynamo.scan(params).promise();
+
+    const statusCode = 200
+
+    const body = JSON.stringify(results.Items)
+
+    const headers = { "Access-Control-Allow-Origin": "*" }
+
+    const response = { statusCode, body, headers };
+
+    return response;
+};
